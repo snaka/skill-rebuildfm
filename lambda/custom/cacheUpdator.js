ea3afd17870451565a698ccc7859d4f0d8198561
snaka <snaka.gml@gmail.com>
2018-08-24 08:11:53 +0900

    移動した関数は削除

--- lambda/custom/cacheUpdator.js
+++ lambda/custom/cacheUpdator.js
@@ -1,49 +1,10 @@
 const assert = require('assert')
-const AWS = require('aws-sdk')
-const request = require('request')
 const podcast = require('podcast')
-const path = require('path')
-
-const s3 = new AWS.S3()
-
-async function fetchEpisode(url) {
-  return new Promise((resolve, reject) => {
-    // 対象が binary の場合 encoding: null としなければならない
-    request.get(url, { encoding: null }, (err, res, body) => {
-      console.log('RESPONSE CODE:', res.statusCode)
-      console.log('RESPONSE HEADERS:', res.headers)
-
-      if (err) {
-        reject(err)
-        return
-      }
-      resolve(body)
-    })
-  })
-}
-
-async function downloadEpisodeToS3(originalUrl) {
-  const episodeBody = await fetchEpisode(originalUrl)
-  const bucket = process.env.S3_BUCKET_EPISODE
-  const key = path.basename(originalUrl)
-
-  try {
-    let head = await s3.headObject({ Bucket: bucket, Key: key }).promise()
-    console.log(`Episode ${key} already downloaded.`)
-    return `https://s3-ap-northeast-1.amazonaws.com/${bucket}/${key}`
-  } catch(err) {
-    console.log('The episode does not exist in S3, the episode will be downloaded.')
-  }
-
-  await s3.putObject({ Bucket: bucket, Key: key, Body: episodeBody }).promise()
-  await s3.putObjectAcl({ Bucket: bucket, Key: key, ACL: 'public-read' }).promise()
-  return `https://s3-ap-northeast-1.amazonaws.com/${bucket}/${key}`
-}
 
 exports.handler = async (event) => {
   assert(process.env.S3_BUCKET_EPISODE, 'S3_BUCKET_EPISODE is not defined.')
 
-  const episode = await podcast.getEpisodeInfo(podcast.config.ID, 0, { useOriginalUrl: true })
+  const episode = await podcast.getEpisodeInfo(podcast.config.ID, 0, { useOriginalScheme: true })
   console.log('episode.url:', episode.url)
 
   return episode.url
