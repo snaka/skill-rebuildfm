4f4ce872e06882597d9a7e7fcae2d0e2484d3e42
snaka <snaka.gml@gmail.com>
2018-08-29 07:50:27 +0900

    cache更新の改善

--- lambda/custom/cacheUpdator.js
+++ lambda/custom/cacheUpdator.js
@@ -4,7 +4,7 @@ const podcast = require('podcast')
 exports.handler = async (event) => {
   assert(process.env.S3_BUCKET_EPISODE, 'S3_BUCKET_EPISODE is not defined.')
 
-  const episode = await podcast.getEpisodeInfo(podcast.config.ID, 0, { useOriginalScheme: true })
+  const episode = await podcast.getEpisodeInfo(podcast.config.ID, 0, { useOriginalScheme: true, forceUseCache: false })
   console.log('episode.url:', episode.url)
 
   return episode.url
